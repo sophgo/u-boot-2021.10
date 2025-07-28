a9962c516a7675bb5d1c72e370fc745453020dc6
ke.yi <ke.yi@sophgo.com>
2025-07-28 15:16:18 +0800

    patch 97723223,add se9b2 pinmux fix uart2 bug
    Change-Id: Ia01247374209d37f3770f19e3b08c15376d0d5b0

--- board/cvitek/cv186x/board.c
+++ board/cvitek/cv186x/board.c
@@ -536,6 +536,8 @@ void set_product_pinmux(void)
 		sm9v1_board_init();
 	else if (strstr(dtstype, "se9b1"))
 		sm9v1_board_init();
+	else if (strstr(dtstype, "se9b2"))
+		sm9v1_board_init();
 	else if (strstr(dtstype, "se9b3"))
 		sm9v1_board_init();
 	else if (strstr(dtstype, "se9b4"))
